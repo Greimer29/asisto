7ef436f0022ad1c7776dfd4e10a328fd3a98d4d6
unknown <greimerabelguzman29@gmail.com>
2024-11-18 18:17:09 -0400

    CRUD de horarios

---
+++ src/pages/admin/SchedulesPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-card class="q-ma-sm text-center">
+    <div class="q-pt-md text-h5 text-bold">Horarios</div>
+    <q-separator inset />
+    <div class="q-pa-md column-in" v-if="!addSchedule">
+      <ScheduleTable @changeComponent="addSchedule = !addSchedule" />
+    </div>
+    <div class="q-pa-md column-in" v-if="addSchedule">
+      <CreateSchedule @changeComponent="addSchedule = !addSchedule" />
+    </div>
+  </q-card>
+</template>
+<script setup>
+import { ref } from "vue";
+import CreateSchedule from "src/components/admin/schdules/CreateSchedule.vue";
+import ScheduleTable from "src/components/admin/schdules/ScheduleTable.vue";
+const addSchedule = ref(false);
+</script>
