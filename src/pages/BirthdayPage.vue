124638c1784d38ce5f2473ea21ef9dc2e54da272
unknown <greimerabelguzman29@gmail.com>
2025-01-08 13:20:40 -0400

    Dise;o y estructura de paginas de eventos

--- src/pages/BirthdayPage.vue
+++ src/pages/BirthdayPage.vue
@@ -1,109 +1,93 @@
 <template>
-  <q-page class="background grid-system">
-    <div class="item item-1"></div>
-    <div class="item item-2"></div>
-    <div class="item item-3"></div>
-    <div class="item item-4 flex flex-center">
-      <q-carousel
-        flat
-        style="
-          max-width: 100vw;
-          min-width: calc(50vw - 25vw);
-          border-radius: 15px;
-          border: 2px solid gold;
-        "
-        animated
-        v-model="slide"
-        control-color="white"
-        navigation
-        swipeable
-        infinite
-        arrows
-        :autoplay="autoplay"
-        transition-prev="slide-right"
-        transition-next="slide-left"
-        @mouseenter="autoplay = false"
-        @mouseleave="autoplay = true"
+  <q-card class="flex flex-center background" style="height: 85vh">
+    <q-carousel
+      class="item"
+      flat
+      style="border-radius: 15px; border: 2px solid gold"
+      animated
+      v-model="slide"
+      control-color="white"
+      navigation
+      infinite
+      arrows
+      :autoplay="autoplay"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+      <q-carousel-slide
+        v-for="(item, index) in birthDudes"
+        :key="index"
+        :name="index"
+        class="bg-black text-white"
       >
-        <!-- <q-carousel-slide
-          v-for="(item, index) in birthDudes"
-          :key="index"
-          :name="index"
-          class="bg-dark"
-        >
-          <q-card flat class="bg-dark full-width full-height">
-            <q-card-section class="text-center">
-              <q-avatar size="200px" font-size="52px">
-                <img src="https://cdn.quasar.dev/img/avatar.png" />
-              </q-avatar>
-            </q-card-section>
-            <q-card-section class="text-center text-white">
-              <div class="text-h5">{{ item.name }} {{ item.lastName }}</div>
-              {{ item.bornDate }}
-            </q-card-section>
-          </q-card>
-        </q-carousel-slide> -->
-        <q-carousel-slide
-          v-for="(item, index) in birthDudes"
-          :key="index"
-          :name="index"
-          class="bg-black text-white"
-        >
-          <div class="column items-center justify-center q-gutter-md">
-            <q-img :src="item.imagen" style="max-width: 200px" />
-            <div class="text-h6">{{ item.name }}</div>
-            <div class="text-subtitle2">Edad: {{ item.age }}</div>
+        <div class="column items-center justify-center q-gutter-md">
+          <q-img :src="item.imagen" style="max-width: 200px" />
+          <div class="text-h6">{{ item.name }} {{ item.lastName }}</div>
+          <div class="text-subtitle1">
+            {{ item.bornDate }}
           </div>
-        </q-carousel-slide>
-      </q-carousel>
-    </div>
-  </q-page>
+          <div class="text-subtitle2">{{ item.age }}</div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+  </q-card>
 </template>
+<script setup>
+import moment from "moment";
+import { api } from "src/boot/axios";
+import { onMounted, ref } from "vue";
 
-<script>
-import { defineComponent, ref } from "vue";
+const birthDudes = ref([]);
+const slide = ref(1);
+const autoplay = ref(true);
+const todayMonth = ref(moment().format("MM"));
+const todaYear = ref(moment().format("YYYY"));
 
-const birthDudes = [
-  { name: "Greimer ", lastName: "Perez", bornDate: "29/06/2001" },
-  { name: "Kenyer ", lastName: "Perez", bornDate: "17/10/1999" },
-  { name: "Tobias ", lastName: "Perez", bornDate: "17/11/1967" },
-];
+const getUsers = async () => {
+  const { data } = await api.get("users");
+  data.forEach((element) => {
+    const [year, month, day] = element.bornDate.split("/");
+    element.age = todaYear.value - year;
+    if (month === todayMonth.value) {
+      birthDudes.value.push(element);
+    }
+  });
+  console.log(birthDudes.value);
+};
 
-export default defineComponent({
-  name: "BirthdayPage",
-  components: {},
-  setup() {
-    return {
-      slide: ref(1),
-      autoplay: ref(true),
-      birthDudes,
-    };
-  },
+onMounted(() => {
+  getUsers();
 });
 </script>
 
 <style scoped>
-.grid-system {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-}
-
-@media (max-width: 650px) {
-  .grid-system {
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(1, 1fr);
-  }
-  .item-1,
-  .item-2 {
-    display: none;
-  }
-}
-
 .background {
-  background-color: black;
-  background-image: url("../../public/images/happyBirthday.jpg");
+  background-image: url("../../public/images/birthday/nosee.jpg");
   background-repeat: no-repeat;
+  background-position: center center;
   background-size: cover;
+}
+
+.item {
+  min-width: 500px;
+  max-width: 100%;
+  position: absolute;
+  left: 280px;
+}
+
+@media (max-width: 980px) {
+  .background {
+    background-image: url("../../public/images/birthday/nosx.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  .item {
+    min-width: 300px;
+    max-width: 100%;
+    position: center;
+    left: auto;
+  }
 }
 </style>
---
+++ src/pages/EventsPage.vue
@@ -0,0 +1,112 @@
+<template>
+  <q-page>
+    <q-tabs
+      class="q-mt-sm"
+      v-model="tabs"
+      indicator-color="amber-7"
+      active-color="amber-7"
+      color="gray"
+      align="justify"
+      narrow-indicator
+    >
+      <q-tab name="events" label="Eventos" />
+      <q-tab name="birthday" label="Cumpleaños" />
+      <q-tab name="efemerides" label="Efemerides" />
+    </q-tabs>
+    <q-tab-panels v-model="tabs" animated>
+      <q-tab-panel name="events">
+        <div>Acompañanos a vivir estos momentos planificados para ti</div>
+        <q-separator />
+        <q-table :rows="events" grid :rows-per-page-options="[0]" hide-bottom>
+          <template v-slot:item="props">
+            <q-card class="events-item q-ma-sm" bordered>
+              <q-card-section class="text-center q-pa-none">
+                <q-img
+                  src="../../public/images/login/maranatha_play.jpeg"
+                  style="height: 200px; max-width: 100%"
+                >
+                  <div class="absolute-bottom text-left">
+                    <div class="text-bold text-subtitle1">
+                      {{ props.row.location }}
+                    </div>
+                  </div>
+                </q-img>
+              </q-card-section>
+
+              <q-card-section class="q-pb-none">
+                <div class="flex">
+                  <div class="q-mr-md">
+                    <q-avatar size="40px"> <q-icon name="event" /> </q-avatar>
+                    {{ props.row.date }}
+                  </div>
+                  <q-space></q-space>
+                  <div class="flex">
+                    <div class="flex flex-center q-mr-sm">
+                      {{ props.row.timeIn }}
+                    </div>
+                    <q-space />
+                    <div class="flex flex-center q-mr-sm">a</div>
+                    <q-space />
+                    <div class="flex flex-center">
+                      {{ props.row.timeOut }}
+                    </div>
+                  </div>
+                </div>
+              </q-card-section>
+              <q-card-section class="q-pt-none">
+                <div class="text-weight-bold text-h5 text-grey-9">
+                  {{ props.row.title }}
+                </div>
+                <div class="text-justify">
+                  <div class="text-subtitle1 text-grey-7 q-py-xs">
+                    {{ props.row.description }}
+                  </div>
+                </div>
+              </q-card-section>
+            </q-card>
+          </template>
+        </q-table>
+      </q-tab-panel>
+      <q-tab-panel name="birthday"> <BirthdayPage /></q-tab-panel>
+      <q-tab-panel name="efemerides"> <EfemeridesPage /> </q-tab-panel>
+    </q-tab-panels>
+  </q-page>
+</template>
+
+<!--  -->
+
+<script setup>
+import { defineComponent, onMounted, ref } from "vue";
+import BirthdayPage from "./BirthdayPage.vue";
+import EfemeridesPage from "./EfemeridesPage.vue";
+import { api } from "src/boot/axios";
+
+const tabs = ref("events");
+const events = ref([]);
+
+const getEvents = async () => {
+  const { data } = await api.get("events");
+  if (data) {
+    events.value = data;
+  }
+};
+
+onMounted(() => {
+  getEvents();
+});
+</script>
+
+<style scoped>
+.events-item {
+  max-width: 400px;
+  min-width: 350px;
+  height: 400px;
+  max-height: 400px;
+}
+@media (max-width: 980px) {
+  .events-item {
+    width: 100%;
+    height: 400px;
+  }
+}
+</style>
